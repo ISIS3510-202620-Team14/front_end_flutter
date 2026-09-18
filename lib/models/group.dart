1577aa2e6d659609d641baf03e3f9a77d3f328d7
Juanesillo <j.trivinon@uniandes.edu.co>
2026-09-18 11:09:20 -0500

    Creation of widgets and model of the view grupos-profesor

---
+++ lib/models/group.dart
@@ -0,0 +1,11 @@
+class Group {
+  final String name;
+  final int childrenCount;
+  final String teacherLabel;
+
+  const Group({
+    required this.name,
+    required this.childrenCount,
+    required this.teacherLabel,
+  });
+}
